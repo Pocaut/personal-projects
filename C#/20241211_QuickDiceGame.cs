587303db3b6341802c7d8fb8d9bdc12f94864bb1
Matheus Pezin Carneiro <mpcmatheus.MP@gmail.com>
2024-12-11 21:48:39 -0300

    Update 20241211_QuickDiceGame.cs

--- C#/20241211_QuickDiceGame.cs
+++ C#/20241211_QuickDiceGame.cs
@@ -14,12 +14,11 @@ if ((roll1==roll2) && (roll1==roll3)){
     total += 6;
 }
 
-else{
-if ((roll1==roll2) || (roll2==roll3) || (roll1==roll3)){
+else if ((roll1==roll2) || (roll2==roll3) || (roll1==roll3)){
 
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
     total += 2;
-} }
+}
 
 Console.WriteLine($@"Dice roll: {roll1} + {roll2} + {roll3} 
 Total = {total}");
