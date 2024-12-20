5f1fb1a301241885c8ac4b156f8c705114747bc9
Matheus Pezin Carneiro <mpcmatheus.MP@gmail.com>
2024-12-19 23:08:33 -0300

    Add files via upload

---
+++ C#/FizzBuzzChallenge.cs
@@ -0,0 +1,26 @@
+﻿int num = 1;
+
+while (num <=100)
+{
+    if(!(num % 3 == 0) && !(num % 5 == 0))
+    {
+        Console.WriteLine($"Num: {num}");
+        num++;
+    }
+
+    else if((num % 3 == 0) && (num % 5 == 0))
+        {
+            Console.WriteLine($"Num: {num} FizzBuzz");
+            num++;
+        }
+    else if(num % 3 == 0)
+        {
+            Console.WriteLine($"Num: {num} Fizz");
+            num++;
+        }
+    else if(num % 5 == 0)
+        {
+            Console.WriteLine($"Num: {num} Buzz");
+            num++;
+        }
+}
