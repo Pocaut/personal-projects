647223a1954d8f734bdcf44ef3e8b0b9d95d3868
Matheus Pezin Carneiro <mpcmatheus.MP@gmail.com>
2025-02-28 07:00:52 -0300

    Add files via upload

---
+++ C#/20250228_MethodReturnValue.cs
@@ -0,0 +1,7 @@
+﻿//Simple practice for Method that returns a value. (i forgot)
+int RandomNumber() {
+    Random random = new Random();
+    return random.Next(1,10);
+}
+
+Console.WriteLine(RandomNumber());
