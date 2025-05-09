d642155ecffde7df0bf0a015cd90de8d2638c58b
Matheus Pezin Carneiro <mpcmatheus.MP@gmail.com>
2025-05-09 08:15:53 -0300

    Create 1twosum.js

---
+++ JS/1twosum.js
@@ -0,0 +1,21 @@
+//solution for the 1 - Two Sum challenge on LeetCode
+const nums = [2,7,11,15];
+const target = 9;
+
+function twoSum(nums, target) {
+
+for (i = 0;i <= 2;i++) {
+let sum = nums[i] + nums[i + 1];
+
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+};
+
+
+console.log(twoSum(nums, target));
